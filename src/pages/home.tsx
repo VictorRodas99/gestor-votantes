82ee4eda93742a4e79f5a37ac52ffe65b1393b08
VictorRodas99 <victormatiasrodas100@gmail.com>
2026-07-16 10:11:17 -0300

    borrar barra de búsqueda

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,9 @@
 import ModuleCard from '../components/module-card'
-import SearchBar from '../components/search-bar'
 import { modules } from '../config/modules'
 
-/**
- * Inicio: buscador rápido por cédula + accesos a los módulos. La barra superior
- * e inferior las aporta `AppLayout`, por eso acá solo va el contenido.
- */
 function HomePage() {
   return (
     <>
-      <SearchBar placeholder="Buscar votante por cédula" />
-
       <div className="mt-5 grid grid-cols-2 gap-4">
         {modules.map((module) => (
           <ModuleCard key={module.key} module={module} />
