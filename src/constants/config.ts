51491bd16dab538be663d6ef728f729d72b107cf
VictorRodas99 <victormatiasrodas100@gmail.com>
2026-07-16 09:47:16 -0300

    definir constantes para las rutas a utilizar y limit

---
+++ src/constants/config.ts
@@ -0,0 +1,5 @@
+export const API_URL: string =
+  import.meta.env.VITE_API_URL ??
+  'https://elecciones.appbinario.com/api/Eleccionesapi'
+
+export const VOTANTES_PER_PAGE = 20
---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const VOTANTE_ROUTES = {
+  index: 'votantes'
+} as const
+
+export const CATALOGO_ROUTES = {
+  localVotacion: 'local_votacion'
+} as const
