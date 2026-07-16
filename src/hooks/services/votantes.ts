8971fa9ed91a43b16e4df06cdd98384294b4f9c9
VictorRodas99 <victormatiasrodas100@gmail.com>
2026-07-16 11:57:56 -0300

    remover refetchOnMount false para evitar tener valores no actualizados a tiempo (ver manera de evitar bombardeo de requests)

--- src/hooks/services/votantes.ts
+++ src/hooks/services/votantes.ts
@@ -33,7 +33,7 @@ export const useVotantesInfinite = (filters: VotantesFilters = {}) => {
       )
       return loaded < lastPage.total ? lastPage.page + 1 : undefined
     },
-    staleTime: VOTANTES_STALE_TIME,
-    refetchOnMount: false
+    staleTime: VOTANTES_STALE_TIME
+    // refetchOnMount: false
   })
 }
