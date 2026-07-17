826ebee8d56bb303fb5c76c478d428b79903696e
VictorRodas99 <victormatiasrodas100@gmail.com>
2026-07-17 18:36:46 -0300

    mostrar paso 3 en console.log + agregar paso tres en el flow

--- src/pages/votante-wizard.tsx
+++ src/pages/votante-wizard.tsx
@@ -3,8 +3,8 @@ import { useState, type ReactNode } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import StepDatosPersonales from '../components/wizard/step-datos-personales'
+import StepDatosVisita from '../components/wizard/step-datos-visita'
 import StepDatosVotacion from '../components/wizard/step-datos-votacion'
-import StepPlaceholder from '../components/wizard/step-placeholder'
 import WizardProgress from '../components/wizard/wizard-progress'
 import votanteWizardFormDefaults from '../forms/votante/default-values'
 import {
@@ -86,6 +86,14 @@ function VotanteWizard() {
       voto_intendente: data.voto_intendente,
       voto_concejal: data.voto_concejal,
       movil: data.movil,
+
+      // paso 3
+      encargado_visita: data.encargado_visita || null,
+      tipo_visita: data.tipo_visita || null,
+      observacion: data.observacion ?? '',
+      familiar: data.familiar,
+      inc: data.inc,
+      valor_inc: data.inc ? data.valor_inc : 0,
       nuevo_referente: data.nuevo_referente
         ? { ...data.nuevo_referente, barrio_id: data.barrio_id ?? 0 }
         : null
@@ -121,13 +129,7 @@ function VotanteWizard() {
           />
         )
       case STEP.visita:
-        return (
-          <StepPlaceholder
-            title="Visita"
-            description="Encargado, tipo de visita y observación."
-            {...stepProps}
-          />
-        )
+        return <StepDatosVisita {...stepProps} />
     }
   }
 
