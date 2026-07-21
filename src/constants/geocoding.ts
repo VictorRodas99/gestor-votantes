85ebeb4a67ea394a3d5e5227c7a3a5bb8822e076
VictorRodas99 <victormatiasrodas100@gmail.com>
2026-07-21 15:25:46 -0300

    definir constantes para búsqueda via nominatim: search limit, max suggestions, debounce ms timeout, etc

--- src/constants/geocoding.ts
+++ src/constants/geocoding.ts
@@ -1,3 +1,9 @@
 export const NOMINATIM_BASE_URL = 'https://nominatim.openstreetmap.org'
-export const NOMINATIM_REVERSE_ZOOM = 1
+export const NOMINATIM_REVERSE_ZOOM = 17
 export const NOMINATIM_LANG = 'es'
+
+export const BUSQUEDA_DEBOUNCE_MS = 600
+export const BUSQUEDA_MIN_CHARS = 3
+
+export const NOMINATIM_SEARCH_LIMIT = 15
+export const SUGERENCIAS_MAX = 5
