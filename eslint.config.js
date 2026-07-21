e3cdbead2f9799e7087ee19661fe21d7d05044ac
VictorRodas99 <victormatiasrodas100@gmail.com>
2026-07-21 19:45:25 -0300

    ignorar directorio api

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'api']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
